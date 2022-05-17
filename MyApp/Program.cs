017abe1ba92001768bc76e21db3f7cc0ecb9e130
SueldoLeandro <sueldoleandro2020@gmail.com>
2022-05-17 15:31:45 -0300

    codigo c#

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese un numero");
+string text = Console.ReadLine();
+Console.WriteLine("Escribio " + texto);
